6874f0c8fdc33e78a4a516f398f03b8bb816aa37
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2023-07-28 19:42:15 +0530

    no need for doing extra stringify res.json did itself

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -37,8 +37,7 @@ router.post("/update", async (req, res) => {
 router.get("/all", async (req, res) => {
   try {
     const products = await fetchProduct();
-    const serializedData = JSON.stringify(products);
-    res.status(200).send(serializedData);
+    res.status(200).json(products);
   } catch (err) {
     console.error("Failed to fetch all products", err);
     res.status(500).json({ status: 0, error: "Failed to fetch all products" });
@@ -50,8 +49,7 @@ router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
     const products = await fetchProduct(id && id);
-    const serializedData = JSON.stringify(products);
-    res.status(200).send(serializedData);
+    res.status(200).send(products);
   } catch (err) {
     console.error(`Failed to fetch id:${id} products`, err);
     res
